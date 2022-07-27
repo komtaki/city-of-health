505316b1d98573b5abb81005db4af11ef99f9626
komtaki <takoreset@gmail.com>
2022-07-27 19:57:04 +0900

    refactor: DataTableコンポーネントを追加

---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,99 @@
+import {
+  DataGrid,
+  GridColDef,
+  GridToolbar,
+  GridRowsProp,
+} from '@mui/x-data-grid'
+
+export const Field = {
+  ranking: 'ranking',
+  prefectureName: 'prefectureName',
+  name: 'name',
+  power: 'power',
+} as const
+
+type Field = keyof typeof Field
+
+const columns = [
+  { field: Field.ranking, headerName: '順位', width: 50 },
+  { field: Field.prefectureName, headerName: '都道府県', width: 130 },
+  { field: Field.name, headerName: '名前', width: 130 },
+  {
+    field: Field.power,
+    headerName: '財政力指数',
+    description:
+      '地方公共団体の財政力を示す指数。基準財政収入額を基準財政需要額で除して得た数値の過去3年間の平均値',
+    type: 'number',
+    width: 130,
+  },
+]
+
+type FinanceWithRanking = Finance & {
+  ranking: number
+}
+
+export const sortAndAddRanking = (data: Finance[]): FinanceWithRanking[] =>
+  data
+    .sort((a: Finance, b: Finance) => {
+      return a.power > b.power ? -1 : 1 // オブジェクトの昇順ソート
+    })
+    .map((datum, i) => ({
+      ...datum,
+      ranking: i + 1,
+    }))
+
+const generateColumns = (fields: Field[]): GridColDef[] =>
+  columns.filter((column) => fields.includes(column.field))
+
+type Props = {
+  fields: Field[]
+  pageSize: number
+  data: GridRowsProp
+  requiredToolBar?: boolean
+}
+
+const DataTable: React.FC<Props> = ({
+  fields,
+  data,
+  pageSize,
+  requiredToolBar = true,
+}) => (
+  <div
+    style={{
+      height: pageSize * 52 + (156 - (requiredToolBar ? 0 : 45)),
+      width: '100%',
+      marginBottom: '3rem',
+    }}
+  >
+    <DataGrid
+      rows={data}
+      columns={generateColumns(fields)}
+      disableColumnFilter
+      disableColumnSelector
+      disableDensitySelector
+      pageSize={pageSize}
+      rowsPerPageOptions={[5]}
+      initialState={{
+        sorting: {
+          sortModel: [{ field: 'power', sort: 'desc' }],
+        },
+      }}
+      components={
+        requiredToolBar
+          ? {
+              Toolbar: GridToolbar,
+            }
+          : {}
+      }
+      componentsProps={
+        requiredToolBar
+          ? {
+              toolbar: { showQuickFilter: true },
+            }
+          : {}
+      }
+    />
+  </div>
+)
+
+export default DataTable
--- src/pages/prefectures/[id].tsx
+++ src/pages/prefectures/[id].tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { NextPage } from 'next'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import Alert from '@mui/material/Alert'
 import AlertTitle from '@mui/material/AlertTitle'
 import Box from '@mui/system/Box'
@@ -11,38 +10,10 @@ import {
   getFinanceByPrefectureName,
 } from '../../lib/api'
 import Layout from '../../components/Layout'
-import DataTable, { columns } from '../../components/DataTable'
+import DataTable, { Field, sortAndAddRanking } from '../../components/DataTable'
 import Text from '../../components/Text'
 import Meta from '../../components/Meta'
 import JapanMap from '../../components/JapanMap'
-
-const columns: GridColDef[] = [
-  { field: 'name', headerName: '名前', width: 130 },
-  {
-    field: 'income',
-    headerName: '歳入総額',
-    description: '地方税などをベースに自治体が得たお金',
-    type: 'number',
-    width: 130,
-  },
-  {
-    field: 'outcome',
-    headerName: '歳出総額',
-    description: '衛生費など自治体が支払ったお金',
-    type: 'number',
-    width: 130,
-  },
-  { field: 'net', headerName: '実質単年度収支', type: 'number', width: 130 },
-  {
-    field: 'health',
-    headerName: '財政健全指数',
-    description: 'This column has a value getter and is not sortable.',
-    type: 'number',
-    width: 130,
-    valueGetter: (params: GridValueGetterParams) =>
-      (params.row.income / params.row.outcome).toFixed(2),
-  },
-]
 
 type Props = {
   data: Finance[]
@@ -50,6 +21,7 @@ type Props = {
 }
 
 const Prefecture: NextPage<Props> = ({ data, prefecture }) => {
+  const pageSize = Math.min(100, data.length)
   return (
     <>
       <Meta
@@ -77,7 +49,7 @@ const Prefecture: NextPage<Props> = ({ data, prefecture }) => {
             <Alert variant="outlined" severity="info">
               <AlertTitle>地方財政状況調査とは</AlertTitle>
 
-              <Text gutterBottom>
+              <Text paragraph>
                 都道府県や市町村など各地方公共団体の決算に関する統計調査で、統一的な会計区分が定められ予算の執行を通じて地方公共団体がどのように行政運営を行ったかを見るものです。
               </Text>
               <Text gutterBottom>
@@ -86,20 +58,12 @@ const Prefecture: NextPage<Props> = ({ data, prefecture }) => {
             </Alert>
           </Box>
         </Box>
-        <div style={{ height: 2775, width: '100%', marginBottom: '3rem' }}>
-          <DataGrid
-            rows={data}
-            columns={columns}
-            pageSize={50}
-            rowsPerPageOptions={[5]}
-            initialState={{
-              sorting: {
-                sortModel: [{ field: 'net', sort: 'desc' }],
-              },
-            }}
-          />
-        </div>
-        <Text variant="h5" component="h2" bold>
+        <DataTable
+          data={data}
+          fields={[Field.ranking, Field.name, Field.power]}
+          pageSize={pageSize}
+        />
+        <Text variant="h2" bold>
           他の都道府県を調べる
         </Text>
         <JapanMap />
@@ -116,7 +80,7 @@ type Params = {
 
 export async function getStaticProps({ params }: Params) {
   const prefecture = getPrefectureById(Number(params.id))
-  const data = getFinanceByPrefectureName(prefecture.name)
+  const data = sortAndAddRanking(getFinanceByPrefectureName(prefecture.name))
   return {
     props: {
       prefecture,
